e29375569af3864ae73c39dcf9d76030f63a0dd7
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-03 19:46:33 -0300

    create app

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,32 @@
 import Menu from './modules/Menu.js';
+import Forca from './modules/Forca.js';
+import FecthData from './modules/FecthData.js';
+import Components from './modules/Components.js';
 
-const menu = new Menu();
+const getData = new FecthData();
+const dataWord = await getData.data('./scripts/modules/data.json');
+const dataCharacter = await getData.data('./scripts/modules/dataImages.json');
 
-//menu.forca.createForca();
+const menu = new Menu(dataWord, dataCharacter);
+const components = new Components();
+let status = false;
 
+// MENU
+
+const startButton = components.createButton('Start', 'button-start');
 menu.handlerMenu();
+
+startButton.addEventListener('click', () => {
+  document.querySelector('main').removeChild(menu.container);
+
+  menu.setCharacter(menu.options.charName);
+  const forca = new Forca('main', menu);
+
+  forca.createForca(menu);
+});
+
+menu.container.appendChild(startButton);
+
 document.querySelector('main').appendChild(menu.container);
 
-console.log(menu);
+//this.forca.finishGame('a', 'a');
