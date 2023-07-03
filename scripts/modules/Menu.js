8c30659315041c408609b5586e6ec0ca89d724a8
WeslleyIvis <weslley_reive@hotmail.com>
2023-07-03 19:47:55 -0300

    menu, modal

--- scripts/modules/Menu.js
+++ scripts/modules/Menu.js
@@ -1,44 +1,40 @@
+import Components from './Components.js';
 import Forca from './Forca.js';
-
 export default class Menu {
-  constructor() {
-    this.forca = new Forca();
-    this.container = this.forca.createNode('section', 'menu');
+  constructor(dataWord, dataCharacter) {
+    this.node = new Components();
+    this.container = this.node.createNode('section', 'menu');
     this.options = {
-      dataWord: [],
-      dataCharacter: [],
+      dataWord: dataWord,
+      dataCharacter: dataCharacter,
       category: 'Random',
-      character: '',
+      charName: '',
+      character: [],
     };
-  }
-
-  async getDataWorlds() {
-    const data = await fetch('./scripts/modules/data.json')
-      .then((r) => r.json())
-      .catch((err) => console.log(err));
-
-    return data;
-  }
-
-  async getDataCharacter() {
-    const data = await fetch('./scripts/modules/dataImages.json')
-      .then((r) => r.json())
-      .catch((err) => console.log(err));
-
-    return data;
   }
 
   setCategory(value) {
     this.options.category = value;
   }
 
-  setCharacter(value) {
-    this.options.dataCharacter.forEach((char) => {
-      if (char.name === value) this.options.character = char;
-    });
+  setCharacter(value = '') {
+    if (value === '' || value === 'Random') {
+      this.options.character =
+        this.options.dataCharacter[
+          Math.floor(Math.random() * this.options.dataCharacter.length)
+        ];
+    } else {
+      this.options.dataCharacter.forEach((char) => {
+        if (char.name === value) this.options.character = char;
+      });
+    }
+
+    return this.options.character;
   }
 
   createMenu() {
+    this.setCharacter('');
+
     const selectCategory = this.select(
       this.options.dataWord,
       'Categoria',
@@ -65,35 +61,24 @@ export default class Menu {
     );
 
     selectCharacter.childNodes[1].addEventListener('change', (event) => {
-      this.setCharacter(event.target.value);
+      this.options.charName = event.target.value;
     });
-
-    const startButton = this.forca.createNode(
-      'button',
-      'button-start',
-      'Iniciar',
-      {
-        att: 'type',
-        value: 'button',
-      },
-    );
-
-    this.startGame(startButton);
 
     this.container.appendChild(selectCategory);
     this.container.appendChild(selectCharacter);
-    this.container.appendChild(startButton);
+
+    return this.container;
   }
 
   select(data, title, selectNode = { id, selectTitle, random }) {
-    let selectContent = this.forca.createNode('div', 'select-box');
+    let selectContent = this.node.createNode('div', 'select-box');
     const label = document.createElement('label');
     label.innerText = title;
     label.setAttribute('for', selectNode.id);
 
-    let customSelect = this.forca.createNode('div', 'custom-select');
+    let customSelect = this.node.createNode('div', 'custom-select');
 
-    let select = this.forca.createSelect(
+    let select = this.node.createSelect(
       data,
       selectNode.id,
       selectNode.selectTitle,
@@ -104,47 +89,37 @@ export default class Menu {
     customSelect.appendChild(select);
     selectContent.appendChild(customSelect);
 
-    console.log(selectContent);
     return selectContent;
   }
 
-  startGame(button) {
-    // const audio = new Audio('./scripts/tom_1.mp3');
-    // audio.play();
+  modalFinishGame(options, word) {
+    const modal = this.node.createNode('div', 'modal-finish');
+    modal.appendChild(this.node.createNode('h2', null, word));
 
-    button.addEventListener('click', () => {
-      document.querySelector('main').removeChild(this.container);
-
-      // Select random character
-      if (this.options.character === '') {
-        this.options.character =
-          this.options.dataCharacter[
-            Math.floor(Math.random() * this.options.dataCharacter.length)
-          ];
-      }
-      // Create Forca game
-      this.forca = new Forca(
-        'main',
-        '',
-        this.options.category,
-        this.options.dataWord,
-        this.options.character,
-      );
-
-      this.forca.createForca();
-      this.options.character = '';
+    const btnC = this.node.createButton('Continua');
+    btnC.addEventListener('click', () => {
+      document
+        .querySelector('main')
+        .removeChild(document.querySelector('main').firstChild);
+      const forca = new Forca('main', options);
+      forca.createForca();
     });
+
+    const btnM = this.node.createButton('Menu');
+    btnM.addEventListener('click', () => {
+      document
+        .querySelector('main')
+        .removeChild(document.querySelector('main').firstChild);
+      document.querySelector('main').appendChild(this.container);
+    });
+
+    modal.appendChild(btnC);
+    modal.appendChild(btnM);
+
+    return modal;
   }
 
-  createInstaceForca(category) {
-    this.forca = new Forca('', category, '');
-    this.forca.createForca();
-  }
-
-  async handlerMenu() {
-    this.options.dataWord = await this.getDataWorlds();
-    this.options.dataCharacter = await this.getDataCharacter();
-
+  handlerMenu() {
     this.createMenu();
   }
 }
